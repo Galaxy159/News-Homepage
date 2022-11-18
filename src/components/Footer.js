fdf1348518f4dafe4fe48b5e4a9b406b2ac81fa3
Galaxy159 <braun.asaf@gmail.com>
2022-11-18 20:59:42 +0200

    setup

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/Galaxy159"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Your Name Here
+        </a>
+        .
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Heading.js
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <>
+      <div>Home New Popular Trending Categories</div>
+    </>
+  );
+}
